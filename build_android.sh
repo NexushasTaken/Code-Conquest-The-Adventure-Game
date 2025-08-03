360e00f1444ea8b0e07f7ee84bb6b9646c258a3d
NexushasTaken <nexushastaken@gmail.com>
2025-08-04 05:44:28 +0800

    add gxbuild scripts

--- build_android.sh
+++ build_android.sh
@@ -9,26 +9,38 @@ set -xe
 
 # NOTE: If you excluded any ABIs in the previous steps, remove them from this list too
 ABIS="arm64-v8a armeabi-v7a x86 x86_64"
-ANDROID_API_VERSION="31"
+API_VERSION="31"
 
-BUILD_TOOLS=android/sdk/build-tools/36.0.0/
-TOOLCHAIN=android/ndk/toolchains/llvm/prebuilt/linux-x86_64
-NATIVE_APP_GLUE=android/ndk/sources/android/native_app_glue
+BUILD=$(pwd)/android/build
+NAME=game
+DEV_NAME=raylib
+SRC=src/*.c
+TARGET=Android
+
+SDK=$(pwd)/android/sdk/$(uname)
+NDK=$(pwd)/android/ndk/$(uname)/android-ndk-r25b
+JAVA=/usr/lib/jvm/java-21-openjdk
+
+BUILD_TOOLS=$SDK/build-tools/36.0.0/
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+NATIVE_APP_GLUE=$NDK/sources/android/native_app_glue
+
+AR=$TOOLCHAIN/bin/llvm-ar
 
 FLAGS="-ffunction-sections -funwind-tables -fstack-protector-strong -fPIC -Wall \
-	-Wformat -Werror=format-security -no-canonical-prefixes \
-	-DANDROID -DPLATFORM_ANDROID -D__ANDROID_API__=${ANDROID_API_VERSION}"
+  -Wa,--noexecstack -Wformat -Werror=format-security -no-canonical-prefixes \
+  -DANDROID -DPLATFORM_ANDROID -D__ANDROID_API__=$API_VERSION"
 
 INCLUDES="-I. -Iinclude -I../include -I$NATIVE_APP_GLUE -I$TOOLCHAIN/sysroot/usr/include"
 
 # Copy icons
-cp assets/icon_ldpi.png android/build/res/drawable-ldpi/icon.png
-cp assets/icon_mdpi.png android/build/res/drawable-mdpi/icon.png
-cp assets/icon_hdpi.png android/build/res/drawable-hdpi/icon.png
-cp assets/icon_xhdpi.png android/build/res/drawable-xhdpi/icon.png
+cp assets/icon_ldpi.png $BUILD/res/drawable-ldpi/icon.png
+cp assets/icon_mdpi.png $BUILD/res/drawable-mdpi/icon.png
+cp assets/icon_hdpi.png $BUILD/res/drawable-hdpi/icon.png
+cp assets/icon_xhdpi.png $BUILD/res/drawable-xhdpi/icon.png
 
 # Copy other assets
-cp assets/* android/build/assets
+cp assets/* $BUILD/assets
 
 # ______________________________________________________________________________
 #
@@ -36,62 +48,69 @@ cp assets/* android/build/assets
 # ______________________________________________________________________________
 
 for ABI in $ABIS; do
-	case "$ABI" in
-		"armeabi-v7a")
-			CCTYPE="armv7a-linux-androideabi"
-			ARCH="arm"
-			LIBPATH="arm-linux-androideabi"
-			ABI_FLAGS="-std=c99 -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
-			;;
+  case "$ABI" in
+    "armeabi-v7a")
+      CCTYPE="armv7a-linux-androideabi"
+      ARCH="arm"
+      ARCH_ALT="$ARCH"
+      LIBPATH="arm-linux-androideabi"
+      ABI_FLAGS="-std=c99 -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
+      ;;
 
-		"arm64-v8a")
-			CCTYPE="aarch64-linux-android"
-			ARCH="aarch64"
-			LIBPATH="aarch64-linux-android"
-			ABI_FLAGS="-std=c99 -target aarch64 -mfix-cortex-a53-835769"
-			;;
+    "arm64-v8a")
+      CCTYPE="aarch64-linux-android"
+      ARCH="aarch64"
+      ARCH_ALT="arm64"
+      LIBPATH="aarch64-linux-android"
+      ABI_FLAGS="-std=c99 -target aarch64 -mfix-cortex-a53-835769"
+      ;;
 
-		"x86")
-			CCTYPE="i686-linux-android"
-			ARCH="i386"
-			LIBPATH="i686-linux-android"
-			ABI_FLAGS=""
-			;;
+    "x86")
+      CCTYPE="i686-linux-android"
+      ARCH="i386"
+      ARCH_ALT="x86"
+      LIBPATH="i686-linux-android"
+      ABI_FLAGS=""
+      ;;
 
-		"x86_64")
-			CCTYPE="x86_64-linux-android"
-			ARCH="x86_64"
-			LIBPATH="x86_64-linux-android"
-			ABI_FLAGS=""
-			;;
-	esac
-  make "lib/$ABI/libraylib.a" -j
+    "x86_64")
+      CCTYPE="x86_64-linux-android"
+      ARCH="x86_64"
+      ARCH_ALT="$ARCH"
+      LIBPATH="x86_64-linux-android"
+      ABI_FLAGS=""
+      ;;
+  esac
+  CC="$TOOLCHAIN/bin/$CCTYPE$API_VERSION-clang"
+  make -C raylib/src clean
+  make -C raylib/src PLATFORM=PLATFORM_ANDROID ANDROID_NDK=$NDK ANDROID_ARCH=$ARCH_ALT ANDROID_API_VERSION=$API_VERSION RAYLIB_BUILD_MODE=RELEASE -j
+  mkdir -p lib/Android/$ABI/
+  mv raylib/src/libraylib.a lib/Android/$ABI/
 
-	CC="$TOOLCHAIN/bin/${CCTYPE}${ANDROID_API_VERSION}-clang"
+  # Compile native app glue
+  # .c -> .o
+  $CC -c $NATIVE_APP_GLUE/android_native_app_glue.c -o $NATIVE_APP_GLUE/native_app_glue.o \
+    $INCLUDES -I$TOOLCHAIN/sysroot/usr/include/$CCTYPE $FLAGS $ABI_FLAGS
 
-	# Compile native app glue
-	# .c -> .o
-	$CC -c $NATIVE_APP_GLUE/android_native_app_glue.c -o $NATIVE_APP_GLUE/native_app_glue.o \
-		$INCLUDES -I$TOOLCHAIN/sysroot/usr/include/$CCTYPE $FLAGS $ABI_FLAGS
+  # .o -> .a
+  $AR rcs lib/$TARGET/$ABI/libnative_app_glue.a $NATIVE_APP_GLUE/native_app_glue.o
 
-	# .o -> .a
-	$TOOLCHAIN/bin/llvm-ar rcs lib/$ABI/libnative_app_glue.a $NATIVE_APP_GLUE/native_app_glue.o
-
-	# Compile project
-	for file in src/*.c; do
-		$CC -c $file -o "$file".o \
-			$INCLUDES -I$TOOLCHAIN/sysroot/usr/include/$CCTYPE $FLAGS $ABI_FLAGS
-	done
-
-        # Link the project with toolchain specific linker to avoid relocations issue.
-	$TOOLCHAIN/bin/ld.lld src/*.o -o android/build/lib/$ABI/libmain.so -shared \
-		--exclude-libs libatomic.a --build-id \
-		-z noexecstack -z relro -z now \
-		--warn-shared-textrel --fatal-warnings -u ANativeActivity_onCreate \
-		"-L$TOOLCHAIN/sysroot/usr/lib/$LIBPATH/${ANDROID_API_VERSION}" \
-		-L$TOOLCHAIN/lib/clang/17/lib/linux/$ARCH \
-		-L. -Landroid/build/obj -Llib/$ABI \
-		-lraylib -lnative_app_glue -llog -landroid -lEGL -lGLESv2 -lOpenSLES  -lc -lm -ldl
+  # Compile project
+  # FLAGS and TYPEFLAGS are from the main build script which sources this one
+  mkdir -p $BUILD/lib/$ABI
+  $CC $SRC -o $BUILD/lib/$ABI/libmain.so -shared \
+  -Wl,--exclude-libs,libatomic.a \
+  -Wl,--build-id \
+  -Wl,-z,noexecstack \
+  -Wl,-z,relro \
+  -Wl,-z,now \
+  -Wl,--warn-shared-textrel \
+  -Wl,--fatal-warnings \
+  -u ANativeActivity_onCreate \
+  "-L$TOOLCHAIN/sysroot/usr/lib/$LIBPATH/$API_VERSION" \
+  -L$TOOLCHAIN/lib/clang/17/lib/linux/$ARCH \
+  -L. -L$BUILD/obj -Llib/$TARGET/$ABI \
+  -lraylib -lnative_app_glue -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lc -lm -ldl
 done
 
 # ______________________________________________________________________________
@@ -100,42 +119,39 @@ done
 # ______________________________________________________________________________
 #
 $BUILD_TOOLS/aapt package -f -m \
-	-S android/build/res -J android/build/src -M android/build/AndroidManifest.xml \
-	-I "android/sdk/platforms/android-${ANDROID_API_VERSION}/android.jar"
+  -S $BUILD/res -J $BUILD/src -M $BUILD/AndroidManifest.xml \
+  -I $SDK/platforms/android-$API_VERSION/android.jar
 
-# Compile .java to .class
-javac -verbose -source 1.8 -target 1.8 \
-  -d android/build/classes \
+# Compile NativeLoader.java
+$JAVA/bin/javac -verbose -source 1.8 -target 1.8 \
   -bootclasspath jre/lib/rt.jar \
-  -classpath "android/sdk/platforms/android-${ANDROID_API_VERSION}/android.jar" \
-  -sourcepath src \
-  android/build/src/com/raylib/game/R.java \
-  android/build/src/com/raylib/game/NativeLoader.java
+  -classpath $SDK/platforms/android-$API_VERSION/android.jar \
+  -sourcepath src $BUILD/src/com/$DEV_NAME/$NAME/R.java \
+  -d $BUILD/classes \
+  $BUILD/src/com/$DEV_NAME/$NAME/NativeLoader.java
 
-# Convert .class to .dex
-$BUILD_TOOLS/d8 --classpath "android/sdk/platforms/android-${ANDROID_API_VERSION}/android.jar" --output android/build/dex ./android/build/classes/com/raylib/game/*.class
+$BUILD_TOOLS/d8 --classpath $SDK/platforms/android-$API_VERSION/android.jar --output $BUILD/dex $BUILD/classes/com/$DEV_NAME/$NAME/*.class
 
 # Add resources and assets to APK
 $BUILD_TOOLS/aapt package -f \
-	-M android/build/AndroidManifest.xml -S android/build/res -A assets \
-	-I "android/sdk/platforms/android-${ANDROID_API_VERSION}/android.jar" -F game.apk android/build/dex
+  -M $BUILD/AndroidManifest.xml -S $BUILD/res -A assets \
+  -I $SDK/platforms/android-$API_VERSION/android.jar -F $NAME.apk $BUILD/dex
 
 # Add libraries to APK
-cd android/build
+cd $BUILD
 for ABI in $ABIS; do
-	../../$BUILD_TOOLS/aapt add ../../game.apk lib/$ABI/libmain.so
+  $BUILD_TOOLS/aapt add ../../$NAME.apk lib/$ABI/libmain.so
 done
 cd ../..
 
+# Sign and zipalign APK
+$BUILD_TOOLS/apksigner sign --ks android/$DEV_NAME.keystore --out $NAME.signed.apk --ks-pass pass:$DEV_NAME $NAME.apk
+mv $NAME.signed.apk $NAME.apk
+
 # Zipalign APK and sign
 # NOTE: If you changed the storepass and keypass in the setup process, change them here too
-$BUILD_TOOLS/zipalign -f 4 game.apk game.final.apk
-mv -f game.final.apk game.apk
+$BUILD_TOOLS/zipalign -f 4 $NAME.apk $NAME.final.apk
+mv -f $NAME.final.apk $NAME.apk
 
-# Install apksigner with `sudo apt install apksigner`
-$BUILD_TOOLS/apksigner sign  --ks android/raylib.keystore --out my-app-release.apk --ks-pass pass:raylib game.apk
-mv my-app-release.apk game.apk
-
-# Install to device or emulator
-android/sdk/platform-tools/adb install -r game.apk
-
+# Install to device or emulator if -r was specified
+[[ "$1" = "-r" ]] && $SDK/platform-tools/adb install -r $NAME.apk || \builtin true
