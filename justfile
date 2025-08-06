1069887f85c6e753ea16eaed319a14b05af58b1a
NexushasTaken <nexushastaken@gmail.com>
2025-08-06 19:25:45 +0800

    add lua

---
+++ justfile
@@ -0,0 +1,24 @@
+configure-linux:
+  cmake -B build-linux -DTARGET=Linux --fresh
+  ln -sf build-linux/compile_commands.json compile_commands.json
+
+#-DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
+#-DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
+configure-android:
+  cmake -B build-android -DTARGET=Android --fresh
+  ln -sf build-android/compile_commands.json compile_commands.json
+
+build-linux:
+  cmake --build build-linux --parallel
+
+build-android:
+  cmake --build build-android --parallel
+
+build-android-make:
+  make TARGET=Android
+
+build-linux-make:
+  make TARGET=Linux -j
+
+run-linux: build-linux
+  ./build-linux/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
-#include "raylib.h"
 #include "raygui.h"
+#include "raylib.h"
 #include <curl/curl.h>
 #include <string>
 
+#define SOL_BUILD_CXX_MODE 1
+#define SOL_ALL_SAFETIES_ON 1
+#include "sol/sol.hpp"
+#include "lua.hpp"
+
 // #define RAYGUI_IMPLEMENTATION
 // #include "raygui.h"
-
-#define CLAY_IMPLEMENTATION
-#include "clay.h"
-#include "./clay_renderer_raylib.c"
-
-void HandleClayErrors(Clay_ErrorData errorData) {
-  // See the Clay_ErrorData struct for more information
-  printf("%s", errorData.errorText.chars);
-  switch (errorData.errorType) {
-    // etc
-  }
-}
 
 int main() {
 #ifdef PLATFORM_DESKTOP
@@ -29,100 +22,16 @@ int main() {
   int SCREEN_HEIGHT = GetScreenHeight();
 #endif
 
-  auto font = GetFontDefault();
+  sol::state lua;
 
-  auto min_mem_size = Clay_MinMemorySize();
-  Clay_Arena arena =
-      Clay_CreateArenaWithCapacityAndMemory(min_mem_size, malloc(min_mem_size));
-  Clay_Initialize(arena, {SCREEN_WIDTH, SCREEN_HEIGHT}, (Clay_ErrorHandler){HandleClayErrors});
-  Clay_SetMeasureTextFunction(Raylib_MeasureText, &font);
+  lua.set_function("print", [] { printf("Hello, World\n"); });
+  auto state = lua.script("print()");
 
   while (!WindowShouldClose()) {
-    Clay_SetLayoutDimensions({GetScreenWidth(), GetScreenHeight()});
-
     BeginDrawing();
     ClearBackground(RAYWHITE);
-
-    Clay_BeginLayout();
-
-    CLAY({
-      .layout = {
-        .sizing = {
-          .width = CLAY_SIZING_GROW(0),
-          .height = CLAY_SIZING_GROW(0),
-        },
-        .childAlignment = {
-          .x = CLAY_ALIGN_X_CENTER,
-          .y = CLAY_ALIGN_Y_CENTER,
-        },
-        .layoutDirection = CLAY_TOP_TO_BOTTOM,
-      },
-      .backgroundColor = {43, 41, 51, 255},
-    }) {
-
-      CLAY({
-        .layout = {
-          .sizing = {
-            .width = CLAY_SIZING_FIT(0),
-            .height = CLAY_SIZING_FIT(0),
-          },
-          .padding = {
-            .left = 8,
-            .right = 8,
-            .top = 8,
-            .bottom = 8,
-          },
-          .childGap = 16,
-          .childAlignment = {
-            .x = CLAY_ALIGN_X_CENTER,
-            .y = CLAY_ALIGN_Y_TOP,
-          },
-        },
-        .backgroundColor = {63, 61, 81, 255},
-      }) {
-
-        CLAY({
-          .layout = {
-            .layoutDirection = CLAY_TOP_TO_BOTTOM,
-          }
-        }) {
-          CLAY_TEXT(CLAY_STRING("Email"), CLAY_TEXT_CONFIG({.textColor = {200, 200, 200, 255}, .fontSize = 40, .letterSpacing = 4}));
-          CLAY_TEXT(CLAY_STRING("Password"), CLAY_TEXT_CONFIG({.textColor = {200, 200, 200, 255}, .fontSize = 40, .letterSpacing = 4}));
-        }
-        CLAY({
-          .layout = {
-            .layoutDirection = CLAY_TOP_TO_BOTTOM,
-          }
-        }) {
-          CLAY_TEXT(CLAY_STRING("your-email"), CLAY_TEXT_CONFIG({.textColor = {200, 200, 200, 255}, .fontSize = 40, .letterSpacing = 4}));
-          CLAY({
-            .id = CLAY_ID("input/password"),
-          }) {
-            CLAY_TEXT(CLAY_STRING("your-password"), CLAY_TEXT_CONFIG({.textColor = {200, 200, 200, 255}, .fontSize = 40, .letterSpacing = 4}));
-            static int index = 0;
-            CLAY({
-              .id = CLAY_ID("input/password/caret"),
-              .layout = {
-                .sizing = {
-                  .width = CLAY_SIZING_FIXED(4),
-                  .height = CLAY_SIZING_GROW(0),
-                },
-              },
-              .backgroundColor = {10, 10, 10, 255},
-              .floating = {
-                .attachTo = CLAY_ATTACH_TO_PARENT,
-              },
-            }){}
-          }
-        }
-      }
-    }
-
-    auto render_cmd_array = Clay_EndLayout();
-    Clay_Raylib_Render(render_cmd_array, &font);
 
     EndDrawing();
   }
   CloseWindow();
 }
-
